353863a5b329a3361452f189c206583d6ae224b7
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-05-09 11:16:50 +0200

    inital_commit

---
+++ vue-boolflix/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HeaderSite/>
+</template>
+
+<script>
+import HeaderSite from "./components/HeaderSite.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HeaderSite,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-boolflix/src/components/HeaderSite.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>prova header</h1>
+</template>
