308f5d1f540b445069a7537195dee86937025b01
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-05-09 11:25:56 +0200

    inital_commit

--- vue-boolflix/src/App.vue
+++ vue-boolflix/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+
   <HeaderSite/>
 </template>
 
--- vue-boolflix/src/components/HeaderSite.vue
+++ vue-boolflix/src/components/HeaderSite.vue
@@ -1,3 +1,14 @@
 <template>
     <h1>prova header</h1>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
